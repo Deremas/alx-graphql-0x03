9ffa1beb3c2fb443d5ba8f9d750a4c2b15e4878f
Dereje Masresha <derejemasresha27@gmail.com>
2025-10-19 21:47:06 +0300

    alx graphql 0x02 project done

--- alx-rick-and-morty-app/pages/_document.tsx
+++ alx-rick-and-morty-app/pages/_document.tsx
@@ -1,10 +1,15 @@
+// pages/_document.tsx
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
+      <Head>
+        {/* Tailwind CDN */}
+        <Script src="https://cdn.tailwindcss.com"></Script>
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
